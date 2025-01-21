5aebdb853e735776ff8f1e80f49c42a40241eb41
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-01-21 20:21:30 +0600

    Add files via upload

---
+++ Nested Loop/1.2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    for(i = 0; i < 2; i++)
+    {
+        printf("One ");
+        for(i = 0; i < 2; i++)
+            printf("Two ");
+    }
+    printf("Val = %d\n", i);
+    return 0;
+}
