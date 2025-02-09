95bc368e2ed6d6e5596887e7c7a4c31c8ab3d983
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-09 12:26:51 +0600

    Add files via upload

---
+++ For Loop/1.3.c
@@ -0,0 +1,14 @@
+//Write a program that displays the product of all odd numbers from 1 to 20.
+#include <stdio.h>
+int main()
+{
+    int i, prod;
+    prod = 1; /* To calculate the product, we initialize the prod
+variable to 1, which is the neutral element of the
+multiplication. */
+    for(i = 1; i < 20; i+=2) /* To calculate the product of odd numbers,
+we increase the step by two. */
+        prod *= i;
+    printf("Product = %d\n", prod);
+    return 0;
+}
