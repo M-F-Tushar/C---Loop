3ba0231b39ff1e471035e48f5ff196b7b59b76c7
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-09 09:19:37 +0600

    Delete For Loop/1.8.c

--- For Loop/1.8.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int i;
-    for(i = 3; i && i-1; i--)//i checks whether i is non-zero.i - 1 checks whether i - 1 is non-zero. The logical AND (&&) operator combines these two conditions. The loop continues if both i and i - 1 are non-zero.
-        printf("%d\n", i);
-    return 0;
-}
